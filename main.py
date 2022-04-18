204c4a3cbeab939aec875cae089d72c2abf8e455
jelcano <jelk24@gmail.com>
2022-04-18 23:20:01 +0200

    main.py changed to a better structure

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ from search_cita import get_cita
 from telegram_integration import telegram_bot_sendtext
 import json
 
+def main() -> None:
+    
+    with open('json_data.json') as json_file:
+        data = json.load(json_file)
 
-with open('json_data.json') as json_file:
-    data = json.load(json_file)
+    token = data['keys']['telegram_token']
+    chatid = data['keys']['telegram_chat_id']
+    id = data['keys']['web_id']
+    cd = data['keys']['web_cd']
+    check = False
 
-token = data['keys']['telegram_token']
-chatid = data['keys']['telegram_chat_id']
-id = data['keys']['web_id']
-cd = data['keys']['web_cd']
-check = False
+    while check == False:
+        check, result = get_cita(id , cd)
+        print(result)
 
-while check == False:
-    check, result = get_cita(id , cd)
-    print(result)
+    bot_message = result
+    telegram_bot_sendtext(bot_message, token, chatid)
 
-bot_message = result
-telegram_bot_sendtext(bot_message, token, chatid)
+
+if __name__ == '__main__':
+    main()
